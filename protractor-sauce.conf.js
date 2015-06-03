62db764c348309063e7d9e4ee6eb5347b96a0f89
Devin Andrews <devand123@gmail.com>
2015-06-03 09:49:55 -0400

    Update protractor-sauce.conf.js

--- protractor-sauce.conf.js
+++ protractor-sauce.conf.js
@@ -5,8 +5,6 @@ exports.config = _.extend(shared_config.config, {
 
     // If sauceUser and sauceKey are specified, seleniumServerJar will be ignored.
     // The tests will be run remotely using SauceLabs.
-    // sauceUser: 'jd_devinandrews',
-    // sauceKey: 'f8d04cf9-2d2c-4b35-bbcd-940f8da479c6',
     sauceUser: process.env.SAUCE_USER,
     sauceKey: process.env.SAUCE_ACCESS_KEY,
     capabilities: {
